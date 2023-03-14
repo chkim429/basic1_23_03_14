4614b642e9a1fef4f8ca1d33149e8bc6747c976c
chkim429 <ckdgjs000@naver.com>
2023-03-14 14:40:15 +0900

    삭제 기능

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,38 +1,145 @@
 package com.ll.basic1;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// @Controller 의 의미
-// 개발자가 스프링부트에게 말한다.
-// 아래 있는 HomeController 는 컨트롤러이다.
+import java.util.*;
+
 @Controller
 public class HomeController {
-   int cnt = 0;
-   public HomeController(){
-       cnt = -1;
-   }
+    private int count;
+    private List<Person> people;
 
+    public HomeController() {
+        count = -1;
+        people = new ArrayList<>();
+    }
+
+    // @GetMapping("/home/main") 의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
     @GetMapping("/home/main")
+    // @ResponseBody 의 의미
+    // 아래 메서드를 실행한 후 그 리턴값을 응답으로 삼아줘
     @ResponseBody
     public String showMain() {
         return "안녕하세요.";
     }
+
     @GetMapping("/home/main2")
     @ResponseBody
     public String showMain2() {
         return "반갑습니다.";
     }
+
     @GetMapping("/home/main3")
     @ResponseBody
     public String showMain3() {
         return "즐거웠습니다.";
     }
+
     @GetMapping("/home/increase")
     @ResponseBody
-    public String showIncrease() {
-        cnt++;
-        return "목표 : "+cnt+"";
+    public int showIncrease() { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
+        count++;
+        return count;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    // @RequestParam 의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+    // @RequestParam 는 생략가능
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        return a + b;
+    }
+
+    @GetMapping("/home/returnBoolean")
+    @ResponseBody
+    public boolean showReturnBoolean() {
+        return true;
+    }
+
+    @GetMapping("/home/returnDouble")
+    @ResponseBody
+    public double showReturnDouble() {
+        return Math.PI;
+    }
+
+    @GetMapping("/home/returnIntArray")
+    @ResponseBody
+    public int[] showReturnIntArray() {
+        int[] arr = new int[]{10, 20, 30};
+
+        return arr;
+    }
+
+    @GetMapping("/home/returnIntList")
+    @ResponseBody
+    public List<Integer> showReturnIntList() {
+        List<Integer> list = new ArrayList<>() {{
+            add(10);
+            add(20);
+            add(30);
+        }};
+
+        return list;
+    }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson(String name, int age) {
+        Person p = new Person(name, age);
+
+        System.out.println(p);
+
+        people.add(p);
+
+        return "%d번 사람이 추가되었습니다.".formatted(p.getId());
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> showPeople() {
+        return people;
+    }
+
+    @GetMapping("/home/removePerson")
+    @ResponseBody
+    public String removePerson(int id){
+        boolean removed = people.removeIf(person -> person.getId() == id);
+
+        if ( removed == false){
+            return "%d번 사람이 존재하지 않습니다".formatted(id);
+        }
+        return "%d번 사람이 삭제되었습니다.".formatted(id);
+    }
+
+}
+
+
+@AllArgsConstructor
+@Getter
+@ToString
+class Person {
+    private static int lastId;
+    private final int id;
+    private final String name;
+    private final int age;
+
+
+    static {
+        lastId = 0;
+    }
+
+    Person(String name, int age) {
+        this(++lastId, name, age);
     }
 }
